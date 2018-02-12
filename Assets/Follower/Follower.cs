b6e3814e5217d70cd35a3d542cc3be80853985ad
Daniel Zuberbuehler <daniel.zuberbuehler@outlook.com>
2018-02-12 21:02:06 +0100

    Added state mnachine for Followers

--- Assets/Follower/Follower.cs
+++ Assets/Follower/Follower.cs
@@ -8,9 +8,11 @@ public class Follower : MonoBehaviour {
     public float aggressionThreshold;
 
     private GameObject player;
+    private Animator stateMachine;
 
     void Start() {
         player = GameObject.FindWithTag("Player");
+        stateMachine = GetComponent<Animator>();
         transform.localRotation = (player.transform.position - transform.position).ToRotation();
     }
 
@@ -20,18 +22,23 @@ public class Follower : MonoBehaviour {
 
     void MoveTowardsPlayer() {
         var playerDirection = player.transform.position - transform.position;
+        stateMachine.SetFloat("Distance", playerDirection.magnitude);
 
         float rotationSpeed = initialRotationSpeed;
         float speed = initialSpeed;
 
-        if (playerDirection.magnitude < aggressionThreshold) {
-            rotationSpeed /= 2;
-            speed *= 2;
+        if (stateMachine.GetCurrentAnimatorStateInfo(0).IsName("Charging")) {
+            rotationSpeed = initialRotationSpeed / 3;
+            speed = initialSpeed * 3;
+        } else if (stateMachine.IsInTransition(0)) {
+            rotationSpeed = initialRotationSpeed * 2;
+            speed = 0;
         }
 
         var rotation = Quaternion.RotateTowards(transform.localRotation, playerDirection.ToRotation(), rotationSpeed * Time.deltaTime);
 
         transform.localRotation = rotation;
         transform.localPosition += rotation.ToDirection() * (speed * Time.deltaTime);
+
     }
 }
